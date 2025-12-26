8920ef3d881789fb0ad627db7b7de1afe902bb9f
moisesfernades16 <moisesfernandes1105@gmail.com>
2025-12-26 17:00:39 -0300

    ajusta formatação e propriedades de estilo no CSS e JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const reveals = document.querySelectorAll('.reveal');
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('visible');
-      } else {
-        entry.target.classList.remove('visible'); // se quiser sumir ao subir
-      }
-    });
-  }, {
-    threshold: 0.2
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible'); 
+    }
   });
+}, {
+  threshold: 0.2
+});
 
-  reveals.forEach(el => observer.observe(el));
+reveals.forEach(el => observer.observe(el));
