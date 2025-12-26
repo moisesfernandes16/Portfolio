2dde9f0b7dc69db209b0894bfb65fd3ab65c64fa
moisesfernades16 <moisesfernandes1105@gmail.com>
2025-12-26 17:53:13 -0300

    corrige estilo do layout e ajusta propriedades de elementos

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const reveals = document.querySelectorAll('.reveal');
+const reveal = document.querySelectorAll(".reveal")
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
-    } else {
-      entry.target.classList.remove('visible'); 
     }
-  });
+    else {
+      entry.target.classList.remove('visible');
+    }
+  })
 }, {
   threshold: 0.2
 });
 
-reveals.forEach(el => observer.observe(el));
+reveal.forEach(el => observer.observe(el));
